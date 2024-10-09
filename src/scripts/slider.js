5e14118ad7306739c5a3338cf9d870653e0eca37
relaxdomi <harmony-falls@protonmail.com>
2024-10-09 23:26:25 +0900

    Photography Lightbox implenetation

--- src/scripts/slider.js
+++
@@ -1,3 +0,0 @@
-gsap.fromTo(".slider-img", {x: -880, duration: 6}, {x: 200, duration: 10, repeat: 0, yoyo: true, yoyoEase: true});
-
-// gsap.to('.slider-img', {x: 260, duration: 10, yoyo: true, yoyoEase: true, repeat: -1});
