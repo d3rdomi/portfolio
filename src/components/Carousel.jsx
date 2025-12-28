e3103a63f6a459cf31965c9effea44fb11c3ed6c
relaxdomi <harmony-falls@protonmail.com>
2025-12-28 21:41:11 +0100

    Bild-Carousel-V2

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -10,8 +10,15 @@ export default function EmblaCarousel({ slides = [] }) {
       <div className="embla__container flex h-full">
         {slides.map((slide, i) => (
           <div className="embla__slide flex-shrink-0 w-full flex items-center justify-center" key={i}>
-            {slide.title && <h2>{slide.title}</h2>}
-            {slide.image && <img src={slide.image} alt={slide.alt || ''} className="object-cover h-full w-full" />}
+            {slide.title && 
+            <h2>{slide.title}</h2>
+            }
+            {slide.text &&
+            <p>{slide.text}</p>
+            }
+            {slide.image && 
+            <img src={slide.image} alt={slide.alt || ''} className="object-cover h-full w-full" />
+            }
           </div>
         ))}
       </div>
