aa903060ccfbecedfec88b2d55e95a2a30cc250b
relaxdomi <harmony-falls@protonmail.com>
2026-01-25 14:21:50 +0100

    Website Titel und Description
    Geänderter Titel und Description der allgemeinen Webseite für SEO optimierung.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Dominik Lange | Portfolio';
-export const SITE_DESCRIPTION = 'Dies ist mein Portfolio';
+export const SITE_TITLE = 'Dominik Lange | Designer aus München';
+export const SITE_DESCRIPTION = 'Dominik Lange – Designer aus München. Mediengestalter mit Fokus auf Design, UX & UI, Motion & Web. Portfolio & Projekte entdecken.';
